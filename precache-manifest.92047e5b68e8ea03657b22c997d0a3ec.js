1ecc685f454b6bb91b402216a216f52938e9a5bf
nasjones <nasjonez1@gmail.com>
2020-05-07 03:13:20 -0400

    Updates

---
+++ precache-manifest.92047e5b68e8ea03657b22c997d0a3ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "544edf1021ba0e996e5627bdf472d3ca",
+    "url": "/static-playlist/index.html"
+  },
+  {
+    "revision": "2ee452b1de7597abede9",
+    "url": "/static-playlist/static/css/main.ce983556.chunk.css"
+  },
+  {
+    "revision": "944a83983450e866a640",
+    "url": "/static-playlist/static/js/2.41964c2d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static-playlist/static/js/2.41964c2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ee452b1de7597abede9",
+    "url": "/static-playlist/static/js/main.3c15aa01.chunk.js"
+  },
+  {
+    "revision": "07b7ae646fd422a205c4",
+    "url": "/static-playlist/static/js/runtime-main.c008594d.js"
+  }
+]);
